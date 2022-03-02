5bb1293393ccece10d0175163ff527054c831732
S Masoom Basha <aahens.m143@gmail.com>
2022-03-02 15:51:12 +0530

    Initial1 commit

---
+++ src/calculator.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class calculator {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int operator= sc.nextInt();
+        switch (operator)
+        {
+            /**
+             * 1--> +
+             * 2--> -
+             * 3--> *
+             * 4--> /
+             * 5--> %
+             */
+            case 1:
+                System.out.println(a+b);
+                break;
+            case 2:
+                System.out.println(a-b);
+                break;
+            case 3:
+                System.out.println(a*b);
+                break;
+            case 4: if(b==0){
+                System.out.println("invalid Division");
+
+            }else{
+                System.out.println(a/b);
+            }
+            break;
+            case 5:if(b==0)
+            {
+                System.out.println("invalid Remainder");
+
+            }else{
+                System.out.println(a%b);
+            }
+            break;
+            default:
+                System.out.println("invalid Operator");
+        }
+    }
+}
